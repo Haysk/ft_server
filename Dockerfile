5c1632497beee615f251e19fd31fffb5906042ef
adylewsk <adylewsk@student.42.fr>
2021-03-12 13:20:06 +0100

    config nginx

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apt-get update && apt-get upgrade -y && apt-get install -y	\
 	php7.3 php7.3-fpm php7.3-mysql php7.3-cli php7.3-common php7.3-json php7.3-xml php7.3-opcache php7.3-readline php7.3-mbstring	\
 	mariadb-server	\
 	wget git curl vim libnss3-tools
+RUN mkdir /var/www/dylews
 ADD srcs/* /root/
 CMD /root/install_all.sh
 
--- srcs/install_all.sh
+++ srcs/install_all.sh
@@ -1,35 +1,37 @@
 # install_nginx
-rm /etc/nginx/sites-enabled/default
-cp /root/nginx_config /etc/nginx/sites-enabled/dylews.com
+# rm /etc/nginx/sites-enabled/default
+cp /root/nginx_config /etc/nginx/sites-available/dylews
+ln -s /etc/nginx/sites-available/dylews /etc/nginx/sites-enabled/
 
 # install_database
 service mysql start
 mysql -u root < /root/database_conf.sql
 
 #install_SSL
-wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
-mv mkcert-v1.4.3-linux-amd64 usr/local/bin/mkcert
-chmod +x /usr/local/bin/mkcert
-mkcert -install
-cd /root/.local/share/mkcert/ && mkcert localhost
+# wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.3/mkcert-v1.4.3-linux-amd64
+# mv mkcert-v1.4.3-linux-amd64 usr/local/bin/mkcert
+# chmod +x /usr/local/bin/mkcert
+# mkcert -install
+# cd /root/.local/share/mkcert/ && mkcert dylews
 
 
 # install_wordpress
 cd
 wget https://fr.wordpress.org/latest-fr_FR.tar.gz
 tar -zxf latest-fr_FR.tar.gz
-mv wordpress /usr/share/nginx/html/
+mv wordpress /var/www/dylews
+mv /root/wp-config.php /var/www/dylews/wordpress/
 
 # install_phpmyadmin
 wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-english.tar.gz
 tar -zxf phpMyAdmin-5.1.0-english.tar.gz
-mv phpMyAdmin-5.1.0-english /usr/share/nginx/html/phpmyadmin
-mv /root/config.inc.php /usr/share/nginx/html/phpmyadmin/.
+mv phpMyAdmin-5.1.0-english /var/www/dylews/phpmyadmin
+mv /root/config.inc.php /var/www/dylews/phpmyadmin/
 
-mv /root/wp-config.php /usr/share/nginx/html/wordpress/.
-chown -R www-data:www-data /usr/share/nginx/html/*
-chmod -R 755 /usr/share/nginx/html/*
+chown -R www-data:www-data /var/www/dylews
+chmod -R 755 /var/www/dylews
+service mysql restart
 service nginx start
 service php7.3-fpm start
 
-# tail -f /var/log/nginx/access.log /var/log/nginx/error.log
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log
